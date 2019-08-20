ba73b15e24a8bd2243bca7b094eb9ff562f3751e
Marcello Silva <marcellovcs@gmail.com>
2019-08-20 17:17:26 -0300

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,22 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => {
+  const submit = event => {
+    event.preventDefault()
+  }
+  return (
+    <div className="container">
+      <div>
+        <h1>Encurte rápidamente sua url e compartilhe!</h1>
+        <div className="inputs">
+          <form onSubmit={submit}>
+            <input required placeholder="url para encurtar"></input>
+            <button>Encurtar</button>
+          </form>
+          <input contentEditable={false} placeholder="url gerada"></input>
+          <button className="copy">Copiar</button>
+        </div>
+      </div>
+    </div>
+  )
+}
